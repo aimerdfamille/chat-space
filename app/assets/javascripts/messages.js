48dd2b63ff67d3e53c9d42986744c57b7a5f8468
aimerdfamille09090 <wataru@takemurawatarunoMacBook-Pro.local>
2019-01-20 16:41:36 +0900

    インデントの修正

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,52 @@
+$(function(){
+	function buildHTML(message){
+		var image = ""
+		if (message.image.url != null) {
+			image = `<img class="hidden" src=${message.image.url}`
+		}
+    var html = `<div class="message">
+                  <div class="top-message">
+                    <div class="top-message__user-name">
+                    ${message.user_name}
+                    </div>
+                    <div class="top-message__time">
+                    ${message.time}
+                    </div>
+                  </div>
+                  <div class="bottom-message">
+                    <p class="bottom-message__body">
+                    ${message.text}
+                    </p>
+                    ${ image }
+                  </div>
+                </div>`
+    return html;
+  }
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $('.form__submit').removeAttr('data-disable-with');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+	  })
+	  .done(function(data){
+	    var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('.form__chat__message').val('');
+      $('.hidden').val('');
+	  })
+	  .fail(function(){
+      alert('error');
+    })
+	})
+
+})
