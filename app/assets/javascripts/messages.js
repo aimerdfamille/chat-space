b0b3a09271f0c77ac80d3ff47f3a77dc5c7f5768
aimerdfamille09090 <wataru@takemurawatarunoMacBook-Pro.local>
2019-01-20 15:17:30 +0900

    余分なコメントアウトの削除

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,6 @@
 $(function(){
 	function buildHTML(message){
 		var image = ""
-		// 画像があれば変数imageにimgタグを挿入
 		if (message.image.url != null) {
 			image = `<img class="hidden" src=${message.image.url}`
 		}
@@ -21,27 +20,6 @@ $(function(){
 										${ image }
 									</div>
 								</div>`
-
-
-
-// <div class="message">
-// <div class="top-message">
-// <div class="top-message__user-name">
-// aaa
-// </div>
-// <div class="top-message__time">
-// 2019/01/20 03:38
-// </div>
-// </div>
-// <div class="bottom-message">
-// <p class="bottom-message__body">
-// test
-// </p>
-// <img class="lower-message__image" src="/uploads/message/image/71/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88_2018-11-02_19.25.13.png" alt="%e3%82%b9%e3%82%af%e3%83%aa%e3%83%bc%e3%83%b3%e3%82%b7%e3%83%a7%e3%83%83%e3%83%88 2018 11 02 19.25.13">
-// </div>
-// </div>
-
-
     return html;
   }
 
