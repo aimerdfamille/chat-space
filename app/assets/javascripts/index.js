1bc78c186fea90a8f0960253e43fa6068b474c82
aimerdfamille09090 <wataru@takemurawatarunoMacBook-Pro.local>
2019-01-22 21:51:03 +0900

    ユーザーサーチをインクリメンタルサーチ化

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,66 @@
+$(function() {
+	function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.user_name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.user_id}" data-user-name="${user.user_name}">追加</a>
+                </div>`
+    return html;
+  }
+
+  function removeUser(name,id){
+    var html = `<div class='chat-group-user clearfix js-chat-member'>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    return  html;
+  }
+
+  $('#user-search-field').on('keyup', function() {
+    var input = $('#user-search-field').val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users) {
+      $('#user-search-result').empty();
+      users.forEach(function(user){
+        var html = appendUser(user);
+        $('#user-search-result').append(html);
+      });
+   	})
+    .fail(function(){
+      alert('ユーザーの検索に失敗しました');
+    })
+  });
+
+  $('#user-search-result').on('click','.chat-group-user__btn--add', function() {
+    var id = $(this).data('user-id');
+    var name  = $(this).data('user-name');
+    var html = removeUser(name,id);
+    $('#chat-group-users').append(html)
+    $(this).parent().empty()
+  });
+
+  $('#chat-group-users').on('click','.js-remove-btn', function() {
+    $(this).parent().empty()
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
